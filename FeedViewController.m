cf94d8e8ca3d29d27c1793cb78c78510ccd7aaca
Joseph L <jxrlee@gmail.com>
2014-06-10 18:13:18 -0700

    Added hyperlinking in text and replaced willhidekeyboard constant with an image height

--- FeedViewController.m
+++ FeedViewController.m
@@ -7,13 +7,15 @@
 //
 
 #import "FeedViewController.h"
-
+#import "TTTAttributedLabel.h"
 
 @interface FeedViewController ()
 @property (weak, nonatomic) IBOutlet UIView *postContainer;
 @property (weak, nonatomic) IBOutlet UIImageView *postImage;
 @property (weak, nonatomic) IBOutlet UIView *actionBar;
 @property (weak, nonatomic) IBOutlet UIImageView *commentImageView;
+@property (weak, nonatomic) IBOutlet UILabel *postText;
+@property (weak, nonatomic) IBOutlet UIImageView *menuBar;
 - (IBAction)onLikeButton:(id)sender;
 - (IBAction)onCommentButton:(id)sender;
 - (IBAction)onShareButton:(id)sender;
@@ -41,7 +43,9 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-
+    
+    
+    // post container
     self.postContainer.layer.cornerRadius = 2;
     self.postImage.layer.cornerRadius = 2;
     self.postContainer.layer.shadowColor = [[UIColor lightGrayColor] CGColor];
@@ -49,8 +53,26 @@
     self.postContainer.layer.shadowRadius = 1;
     self.postContainer.layer.shadowOpacity = 1;
     
+    // like, comment, share bar
     self.actionBar.layer.borderColor = [[UIColor colorWithRed:.5 green:.5 blue:.5 alpha:.1] CGColor];
     self.actionBar.layer.borderWidth = 1;
+
+    NSString *html = @"From collarless shirts to high-waisted pants, <b>#Her</b>'s costume designer, Casey Storm, explains how he created his fashion looks for the future: <a href='http://bit.ly/1jV9zM8'>http://bit.ly/1jV9zM8</a>";
+    
+    NSMutableAttributedString * str = [[NSMutableAttributedString alloc] initWithString:@"http://bit.ly/1jV9zM8"];
+    [str addAttribute: NSLinkAttributeName value: @"http://bit.ly/1jV9zM8" range: NSMakeRange(0, str.length)];
+    
+    // Apply some inline CSS
+    NSString *styledHtml = [self styledHTMLwithHTML:html];
+    
+    // Generate an attributed string from the HTML
+    NSAttributedString *attributedText = [self attributedStringWithHTML:styledHtml];
+    
+    // append
+    [[attributedText mutableCopy] appendAttributedString: str];
+    
+    self.postText.attributedText = attributedText;
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -127,11 +149,20 @@
                           delay:0.0
                         options:(animationCurve << 16)
                      animations:^{
-                         self.commentImageView.frame = CGRectMake(0, self.view.frame.size.height - self.commentImageView.frame.size.height - 44, self.commentImageView.frame.size.width, self.commentImageView.frame.size.height);
+                         self.commentImageView.frame = CGRectMake(0, self.view.frame.size.height - self.commentImageView.frame.size.height - self.menuBar.frame.size.height, self.commentImageView.frame.size.width, self.commentImageView.frame.size.height);
                      }
                      completion:nil];
 }
 
+- (NSString *)styledHTMLwithHTML:(NSString *)HTML {
+    NSString *style = @"<meta charset=\"UTF-8\"><style> body { font-family: 'Helvetica'; font-size: 14px; } b {font-weight: '600'; }</style>";
+    
+    return [NSString stringWithFormat:@"%@%@", style, HTML];
+}
 
+- (NSAttributedString *)attributedStringWithHTML:(NSString *)HTML {
+    NSDictionary *options = @{ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType };
+    return [[NSAttributedString alloc] initWithData:[HTML dataUsingEncoding:NSUTF8StringEncoding] options:options documentAttributes:NULL error:NULL];
+}
 
 @end
